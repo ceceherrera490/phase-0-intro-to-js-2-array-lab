ee1aabd86e94c8223d6de2fba312891210af0e0f
Crystal Akins <ceceherrera490@gmail.com>
2021-10-05 20:50:03 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendCat() {
-  cats.push("Ralph");
+function destructivelyAppendCat(name){
+    cats.push(name)    
 }
 
-function destructivelyPrependCat(name) {
-  cats.unshift(name);
+function destructivelyPrependCat(name){
+    cats.unshift(name) 
 }
 
-function destructivelyRemoveLastCat() {
-  cats.pop();
+function  destructivelyRemoveLastCat (){
+    cats.pop(); 
 }
 
-function destructivelyRemoveFirstCat() {
+function destructivelyRemoveFirstCat(){
   cats.shift();
 }
 
-function appendCat(name) {
-  const newCats = [...cats, "Broom"];
-  return newCats;
+function appendCat(name){
+    let newArr = [...cats];
+     newArr.push(name)
+     return newArr;
 }
 
-function prependCat(name) {
-  const newCatName = ["Arnold", ...cats];
-  return newCatName;
+function prependCat(name){
+  let newArr = [...cats]
+  newArr.unshift(name);
+  return newArr;
 }
 
-function removeLastCat() {
-  const newArrCats = cats.slice(0, cats.length - 1);
-  return newArrCats;
+function removeLastCat(){
+    let newArr = [...cats]
+    newArr.pop();
+    return newArr;
 }
 
-function removeFirstCat() {
-  const newCatRemove = cats.slice(1);
-  return newCatRemove;
+function removeFirstCat(){
+  let newArr = [...cats]
+  newArr.shift();
+  return newArr;
 }
